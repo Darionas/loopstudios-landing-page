5a914a22bc0c37dad65d7b5ce7c01c141bf13f6c
Dario <darionas@gmail.com>
2022-11-23 22:20:42 +0200

    Update README.md_1, index.html_2, main.css_2, and add main.js

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 'Use strict'
 
+    const my_links = document.getElementsByClassName('myLinks')[0];
+    const logo_container = document.getElementsByClassName('logo_container')[0];
+    const hamburger_icon = document.getElementsByClassName('hamburger_icon')[0];
+    const close_icon = document.getElementsByClassName('close_icon')[0];
+    const links_collection = document.getElementsByClassName('myLinks')[0].children;
+    
+/*----upper case of navigation-----*/    
+    for(let i = 0; i < links_collection.length; i++) {
+       const all_links = links_collection[i];
+       all_links.style.textTransform = 'uppercase';
+    }
+
+/*-----toggle hamburger icon------*/
+function toggle_hamburger() {
+    if(my_links.style.display === 'block') {
+        my_links.style.display = 'none';
+        logo_container.style.backgroundColor = 'transparent';
+        hamburger_icon.style.display = 'block';
+        close_icon.style.display = 'none';
+    } else {
+        my_links.style.display = 'block';
+        my_links.style.backgroundColor = 'hsl(0, 0%, 0%)';
+        logo_container.style.backgroundColor = 'hsl(0, 0%, 0%)';
+        hamburger_icon.style.display = 'none';
+        close_icon.style.display = 'block';
+    }
+}
+
