2c755d4dc8385169196b4c318ee19b2043c86d88
Dario <darionas@gmail.com>
2022-11-26 21:55:47 +0200

    Update index.html_5, main.css_5, main.js_1

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 'Use strict'
 
-    const my_links = document.getElementsByClassName('myLinks')[0];
-    const logo_container = document.getElementsByClassName('logo_container')[0];
+    const my_links = document.getElementsByClassName('header_navLinks')[0];
+    const logo_container = document.getElementsByClassName('header_logo_container')[0];
+    const hamburger_container = document.getElementsByClassName('hamburger_container')[0];
     const hamburger_icon = document.getElementsByClassName('hamburger_icon')[0];
     const close_icon = document.getElementsByClassName('close_icon')[0];
-    const links_collection = document.getElementsByClassName('myLinks')[0].children;
+    const links_collection = document.getElementsByClassName('header_navLinks')[0].children;
     
 /*----upper case of navigation-----*/    
     for(let i = 0; i < links_collection.length; i++) {
@@ -13,18 +14,34 @@
     }
 
 /*-----toggle hamburger icon------*/
-function toggle_hamburger() {
-    if(my_links.style.display === 'block') {
+/*---click hamburger-----*/
+hamburger_icon.addEventListener('click', click_hamburger);
+function click_hamburger() {
+    if(my_links.style.display === 'grid') {
         my_links.style.display = 'none';
-        logo_container.style.backgroundColor = 'transparent';
         hamburger_icon.style.display = 'block';
         close_icon.style.display = 'none';
     } else {
-        my_links.style.display = 'block';
+        my_links.style.display = 'grid';
         my_links.style.backgroundColor = 'hsl(0, 0%, 0%)';
         logo_container.style.backgroundColor = 'hsl(0, 0%, 0%)';
+        hamburger_container.style.backgroundColor = 'hsl(0, 0%, 0%)';
         hamburger_icon.style.display = 'none';
         close_icon.style.display = 'block';
     }
 }
 
+/*------click cloce icon------*/
+
+close_icon.addEventListener('click', click_close_icon);
+function click_close_icon() {
+   if(my_links.style.display === 'grid') {
+      my_links.style.display = 'none'
+      logo_container.style.backgroundColor = 'transparent';
+      hamburger_container.style.backgroundColor = 'transparent';
+      hamburger_icon.style.display = 'block';
+      close_icon.style.display = 'none';
+   } else {
+      return;
+   }
+}
