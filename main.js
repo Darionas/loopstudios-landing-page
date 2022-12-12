f44e5a708837458de59d20975ea8ae8e1ebdda66
Dario <darionas@gmail.com>
2022-12-12 21:27:54 +0200

    Update main.css_14, main.js_6

--- main.js
+++ main.js
@@ -5,14 +5,7 @@
     const hamburger_container = document.getElementsByClassName('hamburger_container')[0];
     const hamburger_icon = document.getElementsByClassName('hamburger_icon')[0];
     const close_icon = document.getElementsByClassName('close_icon')[0];
-    const links_collection = document.getElementsByClassName('header_navLinks')[0].children;
     
-
-/*----upper case of navigation-----*/    
-   /* for(let i = 0; i < links_collection.length; i++) {
-       const all_links = links_collection[i];
-       all_links.style.textTransform = 'uppercase';
-    }*/
 
 /*-----toggle hamburger icon------*/
 /*---click hamburger-----*/
