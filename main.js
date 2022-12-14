6248cfcdc4c5a1adb025d6c9bdf2597e0f719bec
Dario <darionas@gmail.com>
2022-12-14 20:46:06 +0200

    Update main.css_16, main.js_7, README.md_5

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 'Use strict'
+/* navigation */
+const my_links = document.getElementsByClassName('header_navLinks')[0];
+const logo_container = document.getElementsByClassName('header_logo_container')[0];
+const hamburger_container = document.getElementsByClassName('hamburger_container')[0];
+const hamburger_icon = document.getElementsByClassName('hamburger_icon')[0];
+const close_icon = document.getElementsByClassName('close_icon')[0];
 
-    const my_links = document.getElementsByClassName('header_navLinks')[0];
-    const logo_container = document.getElementsByClassName('header_logo_container')[0];
-    const hamburger_container = document.getElementsByClassName('hamburger_container')[0];
-    const hamburger_icon = document.getElementsByClassName('hamburger_icon')[0];
-    const close_icon = document.getElementsByClassName('close_icon')[0];
-    
+/* images and images titles */
+const earth = document.getElementsByClassName('earth')[0];
+const earth_title = document.getElementsByClassName('earth_title')[0];
+const arcade = document.getElementsByClassName('arcade')[0];
+const arcade_title = document.getElementsByClassName('arcade_title')[0];
+const soccer = document.getElementsByClassName('soccer')[0];
+const soccer_title = document.getElementsByClassName('soccer_title')[0];
+const race_car = document.getElementsByClassName('race_car')[0];
+const race_title = document.getElementsByClassName('race_title')[0];
+const landscape_photo = document.getElementsByClassName('landscape_photo')[0];
+const landscape_title = document.getElementsByClassName('landscape_title')[0];
+const sky = document.getElementsByClassName('sky')[0];
+const nightSky_title = document.getElementsByClassName('nightSky_title')[0];
+const mars_car = document.getElementsByClassName('mars_car')[0];
+const curiosity_title = document.getElementsByClassName('curiosity_title')[0];
+const fisheye_photo = document.getElementsByClassName('fisheye_photo')[0];
+const fisheye_title = document.getElementsByClassName('fisheye_title')[0];
+
 
 /*-----toggle hamburger icon------*/
 /*---click hamburger-----*/
@@ -26,7 +44,6 @@ function click_hamburger() {
 }
 
 /*------click cloce icon------*/
-
 close_icon.addEventListener('click', click_close_icon);
 function click_close_icon() {
    if(my_links.style.display === 'grid') {
@@ -39,3 +56,68 @@ function click_close_icon() {
       return;
    }
 }
+
+/* Changing images titles text color */
+
+earth.addEventListener('mouseover', () => {
+    earth_title.style.color = 'black';
+})
+earth.addEventListener('mouseleave', () => {
+    earth_title.style.color = 'white';
+})
+
+
+arcade.addEventListener('mouseover', () => {
+    arcade_title.style.color = 'black';
+})
+arcade.addEventListener('mouseleave', () => {
+    arcade_title.style.color = 'white';
+})
+
+
+soccer.addEventListener('mouseover', () => {
+    soccer_title.style.color = 'black';
+})
+soccer.addEventListener('mouseleave', () => {
+    soccer_title.style.color = 'white';
+})
+
+
+race_car.addEventListener('mouseover', () => {
+    race_title.style.color = 'black';
+})
+race_car.addEventListener('mouseleave', () => {
+    race_title.style.color = 'white';
+})
+
+
+landscape_photo.addEventListener('mouseover', () => {
+    landscape_title.style.color = 'black';
+})
+landscape_photo.addEventListener('mouseleave', () => {
+    landscape_title.style.color = 'white';
+})
+
+
+sky.addEventListener('mouseover', () => {
+    nightSky_title.style.color = 'black';
+})
+sky.addEventListener('mouseleave', () => {
+    nightSky_title.style.color = 'white';
+})
+
+
+mars_car.addEventListener('mouseover', () => {
+    curiosity_title.style.color = 'black';
+})
+mars_car.addEventListener('mouseleave', () => {
+    curiosity_title.style.color = 'white';
+})
+
+
+fisheye_photo.addEventListener('mouseover', () => {
+    fisheye_title.style.color = 'black';
+})
+fisheye_photo.addEventListener('mouseleave', () => {
+    fisheye_title.style.color = 'white';
+})
