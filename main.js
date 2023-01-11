3dcaeced54092b82829466487d0e5a6529ea8d0a
Dario <darionas@gmail.com>
2023-01-11 19:48:50 +0200

    final commit

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 'Use strict';
 /* navigation */
-const my_links = document.getElementsByClassName('header_navLinks')[0];
+/* https://xane514.medium.com/aria-controls-for-creating-a-mobile-navbar-6001012153a0 */
 const hamburger_menu = document.querySelector('.hamburger_menu');
-const hamburger_icon = document.getElementsByClassName('hamburger_icon')[0];
-const show_close_icon = document.getElementsByClassName('show_close_icon')[0];
-
-
-
-/* https://stackoverflow.com/questions/4528085/toggle-show-hide-div-with-button*/
 hamburger_menu.addEventListener('click', function() {
    let isExpanded = hamburger_menu.getAttribute('aria-expanded');
    isExpanded === 'true' ? isExpanded = 'false' : isExpanded = 'true';
